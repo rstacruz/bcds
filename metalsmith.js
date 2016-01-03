0b6e560887a005cd0e430b6c349fc6e773655a47
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-04 02:39:58 +0800

    Update images

--- metalsmith.js
+++ metalsmith.js
@@ -3,7 +3,10 @@ var Metalsmith = require('metalsmith')
 var app = Metalsmith(__dirname)
   .source('./src')
   .destination('./public')
-  .metadata({ pretty: true }) // indented jade output
+  .metadata({
+    staticImages: images(),
+    pretty: true // indented jade output
+  })
   .use(require('metalsmith-ignore')('**/_*.*'))
   .use(require('metalsmith-jstransformer')())
   .use(require('metalsmith-sense/sass')())
@@ -12,4 +15,10 @@ if (module.parent) {
   module.exports = app
 } else {
   app.build(function (err) { if (err) throw err })
+}
+
+function images () {
+  var glob = require('glob')
+  return glob.sync('src/images/**/*.{jpg,png}')
+    .map((img) => img.replace(/^src/, ''))
 }
