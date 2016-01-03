d2026b4433bbf4185b6998db2132a8f79c12a6ce
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-01-03 22:08:37 +0800

    Images list

--- metalsmith.js
+++ metalsmith.js
@@ -3,7 +3,7 @@ var Metalsmith = require('metalsmith')
 var app = Metalsmith(__dirname)
   .source('./src')
   .destination('./public')
-  .use(require('metalsmith-sense/html')())
+  .use(require('metalsmith-jstransformer')())
   .use(require('metalsmith-sense/sass')())
 
 if (module.parent) {
